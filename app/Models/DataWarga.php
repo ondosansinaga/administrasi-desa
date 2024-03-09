624cf7a5e7ce9d69368190dcb31a49c60804a8c4
Ondosan Sinaga <ondosan.sinaga@student.uty.ac.id>
2024-03-09 11:54:05 +0700

    Tambah Migration, Model, Controller DataWarga

---
+++ app/Models/DataWarga.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+
+class DataWarga extends Model
+{
+    protected $table = 'data_warga';
+    protected $primaryKey = 'id';
+    protected $keyType = 'int';
+    public $timestamps = true;
+    public $incrementing = true;
+    protected $guarded = ['id'];
+
+    public function news(): HasMany
+    {
+        return $this->hasMany(News::class);
+    }
+
+    public function requestLetters(): HasMany
+    {
+        return $this->hasMany(RequestLetter::class);
+    }
+
+    public function role(): BelongsTo
+    {
+        return $this->belongsTo(Role::class, 'role_id');
+    }
+
+    public function createdBy(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function updatedBy(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'updated_by');
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    
+}
---
+++ database/migrations/2024_03_09_103124_create_data_wargas_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('data_warga', function (Blueprint $table) {
+            $table->id();
+            $table->string('nik')->nullable(false)->unique('nik');
+            $table->string('name')->nullable(false);
+            $table->string('birth_info')->nullable();
+            $table->text('address')->nullable();
+            $table->boolean('gender')->default(false);
+            $table->timestamps();
+            $table->enum('status_1', ['Kelahiran', 'Masuk', 'Keluar','Kematian'])->default('Kelahiran');
+            $table->enum('status_2', ['Aktif', 'Tidak Aktif'])->default('Aktif');
+            $table->enum('status_perkawinana', ['Kawin', 'Belum Kawin'])->default('Belum Kawin');
+            $table->string('job_title')->nullable();
+            $table->string('kewarganegaraan')->nullable();
+
+             // Menambahkan foreign key untuk relasi dengan tabel users
+             $table->unsignedBigInteger('user_id')->nullable();
+             $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
+ 
+             $table->unsignedBigInteger('role_id')->nullable(false);
+             $table->foreign('role_id')->on('roles')->references('id');
+ 
+             $table->unsignedBigInteger('created_by')->nullable();
+             $table->foreign('created_by')->on('users')->references('id')->onDelete('set null');
+ 
+             $table->unsignedBigInteger('updated_by')->nullable();
+             $table->foreign('updated_by')->on('users')->references('id')->onDelete('set null');
+         
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('data_warga');
+    }
+};
